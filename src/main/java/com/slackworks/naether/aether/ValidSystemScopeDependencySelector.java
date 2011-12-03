a0f97a6469a2b26d9f252f5eea185c1ac81a88c1
Michael Guymon <michael.guymon@gmail.com>
2011-12-02 22:24:06 -0500

    add license and javadoc

--- src/main/java/com/slackworks/naether/aether/ValidSystemScopeDependencySelector.java
+++ src/main/java/com/slackworks/naether/aether/ValidSystemScopeDependencySelector.java
@@ -1,5 +1,23 @@
 package com.slackworks.naether.aether;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.File;
 
 import org.slf4j.Logger;
@@ -8,6 +26,14 @@ import org.sonatype.aether.collection.DependencyCollectionContext;
 import org.sonatype.aether.collection.DependencySelector;
 import org.sonatype.aether.graph.Dependency;
 
+/**
+ * {@link DependencySelector} that checks the existence of system scoped 
+ * dependencie's systemPath. If it does not exist on the file system, the 
+ * dependency is excluded. 
+ * 
+ * @author Michael Guymon
+ *
+ */
 public class ValidSystemScopeDependencySelector implements DependencySelector {
 
 	//private static Logger log = LoggerFactory.getLogger(ValidSystemScopeDependencySelector.class);
