30aa87af771491c09f098df802f581dd91d87acf
Michael Guymon <michael.guymon@gmail.com>
2012-04-17 10:41:20 -0400

    File.expand user dir maven repo

--- src/main/ruby/naether/bootstrap.rb
+++ src/main/ruby/naether/bootstrap.rb
@@ -102,7 +102,7 @@ class Naether
       
       def check_local_repo_for_deps(local_repo = nil)
         
-        local_repo = local_repo || ENV['M2_REPO'] || '~/.m2/repository'
+        local_repo = local_repo || ENV['M2_REPO'] || File.expand_path('~/.m2/repository')
         local_repo = File.expand_path(local_repo)
         
         puts "Checking #{local_repo} for jars to bootstrap Naether"
