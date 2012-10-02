7697183906e73ab987b150801a2b0d56fa8de95f
mguymon <mguymon@tobdevoured.com>
2012-10-02 00:51:41 -0400

    remove unfinished felix project

--- felix/src/main/java/com/tobedevoured/naether/osgi/Command.java
+++
@@ -1,7 +0,0 @@
-package com.tobedevoured.naether.osgi;
-
-public interface Command {
-    public String getName();
-    public String getDescription();
-    public boolean execute(String commandline);
-}
--- felix/src/main/java/com/tobedevoured/naether/osgi/Lookup.java
+++
@@ -1,4 +0,0 @@
-package com.tobedevoured.naether.osgi;
-public interface Lookup {
-    public Object lookup(String name);
-}
