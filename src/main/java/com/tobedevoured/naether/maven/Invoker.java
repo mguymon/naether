6f961d684ad3875d617a613b209b4d6a79133382
Michael Guymon <michael.guymon@gmail.com>
2013-10-01 10:30:03 -0400

    add javadocs

--- src/main/java/com/tobedevoured/naether/maven/Invoker.java
+++ src/main/java/com/tobedevoured/naether/maven/Invoker.java
@@ -6,10 +6,18 @@ import org.apache.maven.shared.invoker.*;
 import java.io.File;
 import java.util.Arrays;
 
+/**
+ * Invoke Maven goals
+ */
 public class Invoker {
 
     private DefaultInvoker invoker;
 
+    /**
+     * Construct Invoker
+     * @param localRepo local Maven repo to use
+     * @param mavenHome path to Maven home
+     */
     public Invoker(String localRepo, String mavenHome) {
         invoker = new DefaultInvoker();
         invoker.setLocalRepositoryDirectory( new File(localRepo) );
@@ -18,6 +26,14 @@ public class Invoker {
         }
     }
 
+    /**
+     * Execute goals for a pom
+     *
+     * @param pom String path
+     * @param goals String
+     * @return {@link InvocationResult}
+     * @throws NaetherException
+     */
     public InvocationResult execute(String pom, String... goals) throws NaetherException {
         InvocationRequest request = new DefaultInvocationRequest();
         request.setPomFile(new File(pom));
